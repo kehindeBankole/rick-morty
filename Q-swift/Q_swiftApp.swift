c01368b02fd87ac55e984d69447dfe181cd58b84
kehinde <bankolek1@gmail.com>
2024-12-25 14:06:16 +0100

    Initial Commit

---
+++ Q-swift/Q_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q_swiftApp.swift
+//  Q-swift
+//
+//  Created by Kehinde Bankole on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Q_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
